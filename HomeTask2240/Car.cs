f02fdccf45550792da793b1c9f601b4e09e8d068
muradabdullay3v <murad.abdullazade.53@mail.ru>
2022-04-13 23:09:29 +0400

    created

---
+++ HomeTask2240/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask2240
+{
+    internal class Car
+    {
+        public int HP;
+        public string Name;
+        public string Marka;
+    }
+}
---
+++ HomeTask2240/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace HomeTask2240
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car()
+            {
+                Name = "Mesdes",
+                HP = 272,
+                Marka = "Mesdes"
+            };
+            Directory.CreateDirectory(@"c:\Users\murad\OneDrive\Рабочий стол/NewFolder");
+            File.Create(@"c:\Users\murad\OneDrive\Рабочий стол/NewFolder/test.txt");
+            var Json = JsonConvert.SerializeObject(car);
+            using (StreamWriter streamWriter = new StreamWriter(@"F:\VS pr\HomeTask2240\HomeTask2240\Jeyson.json"))
+            {
+                streamWriter.WriteLine(Json);
+            }
+            string qaqa;
+            using (StreamReader streamReader = new StreamReader(@"F:\VS pr\HomeTask2240\HomeTask2240\Jeyson.json"))
+            {
+                qaqa = streamReader.ReadToEnd();
+            }
+            var Bson = JsonConvert.DeserializeObject(qaqa);
+            Console.WriteLine(Bson);
+        }
+    }
+
+}
